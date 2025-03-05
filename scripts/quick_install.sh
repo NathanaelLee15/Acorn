25b34f34d7c685617a8042d681cca741cb381fe9
NathanaelLee15 <NathanaelLee15@gmail.com>
2025-03-05 12:00:09 -0500

    try setting source vars

--- scripts/quick_install.sh
+++ scripts/quick_install.sh
@@ -10,23 +10,22 @@ echo cloning acorn-plugins
 git clone https://github.com/NathanaelLee15/acorn-plugins.git $HOME/repos/acorn-plugins
 git pull "$HOME/repos/acorn-plugins"
 
-# echo setting user env ACORN_PATH
-# setx ACORN_PATH $HOME/repos/Acorn
-# set ACORN_PATH=$HOME/repos/Acorn
+echo "setting ACORN_PATH:  $HOME/repos/Acorn"
+export ACORN_PATH=$HOME/repos/Acorn
 
-# echo adding Acorn/bin to PATH
-# echo %PATH%|findstr "Acorn" >nul || setx PATH "%PATH%;%ACORN_PATH%/bin"
+echo "adding Acorn/bin to PATH:  $HOME/repos/Acorn/bin"
+export PATH="$PATH;$HOME/repos/Acorn/bin"
 
 # # cd to Acorn
 # cd %ACORN_PATH%
 
 # echo installing bun
-# call scripts/install_bun.bat
+# call scripts/install_bun.sh
 
 # echo running bun install
 # call bun i
 
 # echo bootstrapping repo
-# call scripts/bootstrap.bat
+# call scripts/bootstrap.sh
 
 # code .
