005bed2d660d90767e56d648d46bcd2d43e9e755
NathanaelLee15 <NathanaelLee15@gmail.com>
2025-03-05 11:31:47 -0500

    add nix installer

---
+++ scripts/quick_install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Acorn Quick Install";
+
+echo $HOME
+
+
+# echo cloning Acorn
+# git clone https://github.com/NathanaelLee15/Acorn.git C:/Users/%USERNAME%/repos/Acorn
+# git pull $HOME/repos/Acorn
+
+# echo cloning acorn-plugins
+# git clone https://github.com/NathanaelLee15/acorn-plugins.git C:/Users/%USERNAME%/repos/acorn-plugins
+# git pull C:/Users/%USERNAME%/repos/acorn-plugins
+
+# echo setting user env ACORN_PATH
+# @REM use /m for system-wide
+# setx ACORN_PATH C:/Users/%USERNAME%/repos/Acorn
+# set ACORN_PATH=C:/Users/%USERNAME%/repos/Acorn
+
+# @REM echo adding Acorn/bin to PATH
+# @REM echo %PATH%|findstr "Acorn" >nul || setx PATH "%PATH%;%ACORN_PATH%/bin"
+
+# @REM cd to Acorn
+# cd %ACORN_PATH%
+
+# echo installing bun
+# call scripts/install_bun.bat
+
+# echo running bun install
+# call bun i
+
+# echo bootstrapping repo
+# call scripts/bootstrap.bat
+
+# code .
