1f846001ac7d72363f48cb9262f7ba0300698887
NathanaelLee15 <NathanaelLee15@gmail.com>
2025-03-05 11:54:05 -0500

    clone repos

--- scripts/quick_install.sh
+++ scripts/quick_install.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-echo "Acorn Quick Install";
+echo "Acorn Quick Install"
 
-echo $HOME
+echo "cloning Acorn"
+git clone https://github.com/NathanaelLee15/Acorn.git $HOME/repos/Acorn
+git pull "$HOME/repos/Acorn"
 
-
-# echo cloning Acorn
-# git clone https://github.com/NathanaelLee15/Acorn.git C:/Users/%USERNAME%/repos/Acorn
-# git pull $HOME/repos/Acorn
-
-# echo cloning acorn-plugins
-# git clone https://github.com/NathanaelLee15/acorn-plugins.git C:/Users/%USERNAME%/repos/acorn-plugins
-# git pull C:/Users/%USERNAME%/repos/acorn-plugins
+echo cloning acorn-plugins
+git clone https://github.com/NathanaelLee15/acorn-plugins.git $HOME/repos/acorn-plugins
+git pull "$HOME/repos/acorn-plugins"
 
 # echo setting user env ACORN_PATH
-# @REM use /m for system-wide
-# setx ACORN_PATH C:/Users/%USERNAME%/repos/Acorn
-# set ACORN_PATH=C:/Users/%USERNAME%/repos/Acorn
+# setx ACORN_PATH $HOME/repos/Acorn
+# set ACORN_PATH=$HOME/repos/Acorn
 
-# @REM echo adding Acorn/bin to PATH
-# @REM echo %PATH%|findstr "Acorn" >nul || setx PATH "%PATH%;%ACORN_PATH%/bin"
+# echo adding Acorn/bin to PATH
+# echo %PATH%|findstr "Acorn" >nul || setx PATH "%PATH%;%ACORN_PATH%/bin"
 
-# @REM cd to Acorn
+# # cd to Acorn
 # cd %ACORN_PATH%
 
 # echo installing bun
