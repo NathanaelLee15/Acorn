5eda018b6e7c8f3777a031e2f538ee1ba7df7c24
NathanaelLee15 <NathanaelLee15@gmail.com>
2025-03-05 12:34:22 -0500

    reorder

--- scripts/quick_install.sh
+++ scripts/quick_install.sh
@@ -2,13 +2,7 @@
 
 echo "Acorn Quick Install"
 
-echo "cloning Acorn"
-git clone https://github.com/NathanaelLee15/Acorn.git $HOME/repos/Acorn
-git pull $HOME/repos/Acorn
-
-echo cloning acorn-plugins
-git clone https://github.com/NathanaelLee15/acorn-plugins.git $HOME/repos/acorn-plugins
-git pull $HOME/repos/acorn-plugins
+cwd=`pwd`
 
 echo "setting ACORN_PATH:  $HOME/repos/Acorn"
 export ACORN_PATH="$HOME/repos/Acorn"
@@ -17,17 +11,31 @@ echo "adding Acorn/bin to PATH:  $HOME/repos/Acorn/bin"
 export PATH="$PATH;$HOME/repos/Acorn/bin"
 echo "$PATH"
 
+echo cloning acorn-plugins
+git clone https://github.com/NathanaelLee15/acorn-plugins.git $HOME/repos/acorn-plugins
+cd $HOME/repos/acorn-plugins
+git pull
+
+cd $cwd
+
+echo "cloning Acorn"
+git clone https://github.com/NathanaelLee15/Acorn.git $ACORN_PATH
 cd $ACORN_PATH
+git pull
 
 echo "installing bun"
-chmod +x ./scripts/install_bun.sh
-./scripts/install_bun.sh
+if [ -f ./scripts/install_bun.sh ]; then
+    chmod +x ./scripts/install_bun.sh
+    ./scripts/install_bun.sh
+fi
 
 echo "running bun install"
 bun install
 
 echo "bootstrapping repo"
-chmod +x ./scripts/bootstrap.sh
-./scripts/bootstrap.sh
+if [ -f ./scripts/bootstrap.sh ]; then
+    chmod +x ./scripts/bootstrap.sh
+    ./scripts/bootstrap.sh
+fi
 
 code .
